4d3662b8f02bb6deb584cfe3e44174df9e40627f
Lukas Theis <muwum@muwum.net>
2022-03-15 23:56:14 +0100

    iprove limitations for local builds

--- data/update-builds-json.js
+++ data/update-builds-json.js
@@ -1,11 +1,13 @@
 const path = require("path");
 const urllib = require("urllib");
 const fs = require("fs-extra");
+const {app: electronApp} = require("electron");
 
 module.exports = async({baseConfig}) => {
   const buildJsonPath = path.resolve(baseConfig.processDir, "builds.json");
+
   const localBuilds = path.resolve(process.cwd(), "../gw2-raid-builds/builds.json");
-  if (await fs.pathExists(localBuilds)) {
+  if (!electronApp.isPackaged && await fs.pathExists(localBuilds)) {
     baseConfig.buildJsonPath = localBuilds;
     baseConfig.localBuilds = true;
   } else {
