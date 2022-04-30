5e284f4a5772cdf9323f1604c9e0b3417bb97cc2
Lukas Theis <muwum@muwum.net>
2022-04-30 22:15:40 +0200

    compress using stream

--- data/arc-interface/handle-compress.js
+++ data/arc-interface/handle-compress.js
@@ -2,15 +2,18 @@ const path = require("path");
 const fs = require("fs-extra");
 const zlib = require("zlib");
 const {promisify} = require("util");
-
-const zip = promisify(zlib.deflate);
+const {pipeline} = require("stream");
+const pipe = promisify(pipeline);
 
 module.exports = async function handleCompress(logsPath, compressEntries) {
   while (compressEntries.length > 0) {
     const uncompressedJSON = compressEntries.shift();
     console.info(`compressing: ${uncompressedJSON}`);
-    const content = await fs.readFile(path.join(logsPath, uncompressedJSON));
-    await fs.outputFile(path.join(logsPath, `${uncompressedJSON}z`), await zip(content));
+
+    const zipStream = zlib.createDeflate();
+    const source = fs.createReadStream(path.join(logsPath, uncompressedJSON));
+    const destination = fs.createWriteStream(path.join(logsPath, `${uncompressedJSON}z`));
+    await pipe(source, zipStream, destination);
     await fs.remove(path.join(logsPath, uncompressedJSON));
   }
 };
